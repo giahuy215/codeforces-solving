b2b2e8c810213235898f82b7e0b4b9bc299a6487
giahuy215 <giahuy4842@gmail.com>
2023-11-21 23:02:23 +0700

    add url and topic for problem 1883A

--- ProblemA/problem1883A.cpp
+++ ProblemA/problem1883A.cpp
@@ -1,3 +1,26 @@
+// https://codeforces.com/problemset/problem/1883/A
+// A. Morning
+// time limit per test: 2 seconds
+// memory limit per test: 256 megabytes
+// input: standard input
+// output: standard output
+// You are given a four-digit pin code consisting of digits 0 to 9 that needs to be entered. Initially, the cursor points to the digit 1.
+// In one second, you can perform exactly one of the following two actions:
+
+// - Press the cursor to display the current digit,
+// - Move the cursor to any adjacent digit.
+
+// 1 2 3 4 5 6 7 8 9 0 (image)
+// The image above shows the device you are using to enter the pin code. For example, for the digit 5, the adjacent digits are 4 and 6,
+// and for the digit 0, there is only one adjacent digit, 9. Determine the minimum number of seconds required to enter the given four-digit pin code.
+
+// Input
+// Each test consists of multiple test cases. The first line contains a single integer t(1 <= t <= 10^4) - the number of the test cases. 
+// This is followed by their description.
+// The single line of each test case describes the pin code as a string of length 4, consisting of digits from 0 to 9.
+
+// Output
+// For each test case, output the minimum number of seconds required to enter the given pin code.
 #include <bits/stdc++.h>
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
 #define ROF(i, a, b) for (int i = (b-1); i >= (a); i--)
