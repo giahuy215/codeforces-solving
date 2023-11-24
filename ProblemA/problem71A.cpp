91580d32dfa197aeaaca059ab5345a894ae674d3
giahuy215 <giahuy4842@gmail.com>
2023-11-24 21:11:05 +0700

    solving problem 71A

--- ProblemA/problem71A.cpp
+++ ProblemA/problem71A.cpp
@@ -1,9 +1,9 @@
 // https://codeforces.com/problemset/problem/71/A
 // A. Way Too Long Words
-// time limit per test1 second
-// memory limit per test256 megabytes
-// inputstandard input
-// outputstandard output
+// time limit per test: 1 second
+// memory limit per test: 256 megabytes
+// input: standard input
+// output: standard output
 // Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
 
 // Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
