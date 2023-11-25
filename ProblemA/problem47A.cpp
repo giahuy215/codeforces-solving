4eea707e934da8420996f84cb8857cf2fae9f350
giahuy215 <giahuy4842@gmail.com>
2023-11-26 01:10:04 +0700

    solving problem 47A

--- ProblemA/problem47A.cpp
+++ ProblemA/problem47A.cpp
@@ -1,3 +1,21 @@
+// https://codeforces.com/problemset/problem/47/A
+// A. Triangular numbers
+// time limit per test: 2 seconds
+// memory limit per test: 256 megabytes
+// input: standard input
+// output: standard output
+// A triangular number is the number of dots in an equilateral triangle uniformly filled with dots. 
+// For example, three dots can be arranged in a triangle; thus three is a triangular number. 
+// The n-th triangular number is the number of dots in a triangle with n dots on a side. Tn = n*(n+1)/2 . 
+// You can learn more about these numbers from Wikipedia (http://en.wikipedia.org/wiki/Triangular_number).
+
+// Your task is to find out if a given integer is a triangular number.
+
+// Input
+// The first line contains the single number n (1 ≤ n ≤ 500) — the given integer.
+
+// Output
+// If the given integer is a triangular number output YES, otherwise output NO.
 #include <bits/stdc++.h>
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
 #define ROF(i, a, b) for (int i = (b-1); i >= (a); i--)
