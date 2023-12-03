c71bd2632b955d1b4f6ed8779869171166d42fbe
giahuy215 <giahuy4842@gmail.com>
2023-12-03 21:02:22 +0700

    solved problem 1873 B

--- ProblemB/problem1873B.cpp
+++ ProblemB/problem1873B.cpp
@@ -1,8 +1,26 @@
+// https://codeforces.com/contest/1873/problem/B
+// B. Good Kid
+// time limit per test: 1 second
+// memory limit per test: 256 megabytes
+// input: standard input
+// output: standard output
+// Slavic is preparing a present for a friend's birthday. He has an array a of n digits and the present will be the product of all these digits. 
+// Because Slavic is a good kid who wants to make the biggest product possible, he wants to add 1 to exactly one of his digits.
+
+// What is the maximum product Slavic can make?
+
+// Input
+// The first line contains a single integer t (1 ≤ t ≤ 10^4) — the number of test cases.
+
+// The first line of each test case contains a single integer n (1 ≤ n ≤ 9) — the number of digits.
+
+// The second line of each test case contains n space-separated integers ai (0 ≤ ai ≤ 9) — the digits in the array.
+
+// Output
+// For each test case, output a single integer — the maximum product Slavic can make, by adding 1 to exactly one of his digits.
 #include <bits/stdc++.h>
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
-#define ROF(i, a, b) for (int i = (b-1); i >= (a); i--)
 
-#define all(x) (x).begin(), (x).end()
 typedef long long ll;
 using namespace std;
 
