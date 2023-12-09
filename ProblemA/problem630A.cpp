c44d4629c1c7f0f52ecdc2eb39079bd6645ed265
giahuy215 <giahuy4842@gmail.com>
2023-12-09 17:15:05 +0700

    solved problem 630 A

---
+++ ProblemA/problem630A.cpp
@@ -0,0 +1,33 @@
+// https://codeforces.com/problemset/problem/630/A
+// A. Again Twenty Five!
+// time limit per test: 0.5 seconds
+// memory limit per test: 64 megabytes
+// input: standard input
+// output: standard output
+
+// The HR manager was disappointed again. The last applicant failed the interview the same way as 24 previous ones. 
+// "Do I give such a hard task?" — the HR manager thought. "Just raise number 5 to the power of n and get last two digits of the number. 
+// Yes, of course, n can be rather big, and one cannot find the power using a calculator, but we need people who are able to think, 
+// not just follow the instructions."
+
+// Could you pass the interview in the machine vision company in IT City?
+
+// Input
+// The only line of the input contains a single integer n (2 ≤ n ≤ 2·10^18) — the power in which you need to raise number 5.
+
+// Output
+// Output the last two digits of 5^n without spaces between them.
+#include <bits/stdc++.h>
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  ll n;
+  cin >> n;
+  cout << 25;
+  return 0;
+}
