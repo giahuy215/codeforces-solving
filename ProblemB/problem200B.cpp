20413493ecb03f5004889dc516ed5ed88eddb060
giahuy215 <giahuy4842@gmail.com>
2023-12-15 19:09:36 +0700

    solved problem 200B

--- ProblemB/problem200B.cpp
+++ ProblemB/problem200B.cpp
@@ -1,3 +1,24 @@
+// https://codeforces.com/problemset/problem/200/B
+// time limit per test: 2 seconds
+// memory limit per test: 256 megabytes
+// input: standard input
+// output: standard output
+// Little Vasya loves orange juice very much. That's why any food and drink in his kitchen necessarily contains orange juice. 
+// There are n drinks in his fridge, the volume fraction of orange juice in the i-th drink equals pi percent.
+
+// One day Vasya decided to make himself an orange cocktail. He took equal proportions of each of the n drinks and mixed them. 
+// Then he wondered, how much orange juice the cocktail has.
+
+// Find the volume fraction of orange juice in the final drink.
+
+// Input
+// The first input line contains a single integer n (1 ≤ n ≤ 100) — the number of orange-containing drinks in Vasya's fridge. 
+// The second line contains n integers pi (0 ≤ pi ≤ 100) — the volume fraction of orange juice in the i-th drink, in percent. 
+// The numbers are separated by a space.
+
+// Output
+// Print the volume fraction in percent of orange juice in Vasya's cocktail. 
+// The answer will be considered correct if the absolute or relative error does not exceed 1e-4.
 #include <bits/stdc++.h>
 #define FOR(i, a, b) for (int i = (a); i < (b); i++)
 
